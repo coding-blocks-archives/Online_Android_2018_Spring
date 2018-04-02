cfbb8280699c7631d891c38c506b5af2c8835c8d
Arnav Gupta <arnav@codingblocks.com>
2018-04-03 00:25:17 +0530

    fragments
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ 12_Fragments/StaticFragments/app/src/main/java/com/codingblocks/staticfragments/CountUpFragment.kt
@@ -0,0 +1,32 @@
+package com.codingblocks.staticfragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_count_up.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class CountUpFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var count = 0
+        val fragmentView = inflater.inflate(R.layout.fragment_count_up, container, false)
+
+        fragmentView.btnAdd.setOnClickListener {
+            count++
+            fragmentView.tvCounter.text = count.toString()
+        }
+
+        return fragmentView
+    }
+
+
+}
